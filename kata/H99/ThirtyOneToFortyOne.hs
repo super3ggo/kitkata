1987cf91e311caf8aaf4f23ad665f1b8baa0adaf
super3ggo <14225024+super3ggo@users.noreply.github.com>
2020-12-03 23:50:43 -0600

    Add H-99 problems ThirtyOneToFortyOne.
    ;|

---
+++ kata/H99/ThirtyOneToFortyOne.hs
@@ -0,0 +1,10 @@
+-- Filename: ThirtyOneToFortyOne.hs
+-- Note:     Solutions to H99 problems 31 through 41.
+--
+--           Didn't really feel like doing any of these problems.
+
+module H99.ThirtyOneToFortyOne where
+
+complain :: IO ()
+complain = do
+  putStrLn "But I don't wanna to do more math problems with Haskell!"
